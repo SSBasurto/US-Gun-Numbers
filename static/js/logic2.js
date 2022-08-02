a93fbafa3831e5edb5df6c7df4868f9dfa2df851
Apalacios <Apalacios0020@gmail.com>
2022-08-01 19:09:53 -0700

    updated

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -12,23 +12,26 @@ const geo = './static/data/geo_states.json'
 
 /* state color json */
 const color = './static/data/state_color.json'
-// let color = './static/data/state_color.csv'
 
 /* State Center Points */
-// const center = './static/data/center_points.json'
 const center = './static/data/center_points.csv'
 
+/* Guns */
+const gun = './static/data/guns2.csv'
 
-Promise.all([d3.json(geo),d3.json(color),d3.csv(center)]).then(function(data) {
+/* D3 */
+Promise.all([d3.json(geo),d3.json(color),d3.csv(center),d3.csv(gun)])
+                .then(function(data) {
 
     // Data
     const features = data[0].features
     const color = data[1]
     const centers = data[2]
+    const guns = data[3]
     console.log('States Features:',features);
     console.log('State Color:', color); 
     console.log('center:', centers);
-    console.log('center:', centers[0]['Longitude']);    
+    console.log('Guns:', guns);    
     
     // Appending Blue and Red States
     let blue = [];
@@ -44,19 +47,43 @@ Promise.all([d3.json(geo),d3.json(color),d3.csv(center)]).then(function(data) {
         if (blue.includes(name) ) return "blue";
         else return "red";
         };
-    console.log('blue:', blue);
-    
-    // Geo Json
+        
+    // Geo Json and Color function COME BACK TO LAYER  newLayer =
     L.geoJson(data[0], {
         style: function(feature) {
             return {
               color: "white",
               fillColor: chooseColor(feature.properties.NAME),
-              fillOpacity: 0.40,
-              weight: 1.30
+              fillOpacity: 0.65,
+              weight: 0.85
             };
         }
 
     }).addTo(myMap);
 
+    // Marker lists
+    let c_coordinates = []
+    let markers = []
+    let circles = []
+    
+    // Markers w/ center data
+    for (let i = 0; i < centers.length; i++) {
+        c_coordinates.push([centers[i]['Latitude'],centers[i]['Longitude']]);
+    
+        // markers.push (
+            L.marker(c_coordinates[i], {                
+                }).bindPopup(`<h1>${guns[i]['State']}</h1><hr>
+                    <h3>Population: ${guns[i]['Population']}</H3>
+                    <h3>Registered Guns: ${guns[i]['Registerd Guns']}</H3>
+                    <h3>Pop. Gun Ownership: ${guns[i]['Gun Ownership']}</H3>
+                    <h3>School Shootings: ${guns[i]['School Shootings']}</H3>`)
+                        .addTo(myMap);
+        // )
+        // circles.push(
+            L.circle(c_coordinates[i], {
+
+            })
+        // )
+
+    }
 });
